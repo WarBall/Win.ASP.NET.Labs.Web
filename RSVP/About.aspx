74ca10f158de52e7db4b461b6156af7d46da3a26
WarBall <desik_t9@mail.ru>
2021-03-15 21:23:06 +0300

    Add project files.

---
+++ RSVP/About.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="" Language="C#" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="RSVP.About" MasterPageFile="~/Site.Master" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <h1>Семинар</h1>
+    <h2>Приглашаем на семинар</h2>
+    <p>
+        Напоминаем Вам, что если Вы планируете выступить с докладом, то презентация для
+        доклада должна быть загружена до начала работы секции
+    </p>
+    <p><% DateTime dateTime = new DateTime(2021, 1, 1, 9, 0, 0); %>
+        Начало регистрации <%=dateTime.ToString("MM/dd/yyyy HH:mm") %></p>
+</asp:Content>
---
+++ RSVP/Global.asax.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace RSVP
+{
+    public class Global : System.Web.HttpApplication
+    {
+        #region Events not involved
+
+
+
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        protected void Application_OnEndRequest()
+        {
+            Response.Write("<hr />Эта страница была загружена " + DateTime.Now.ToString());
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            Application["Visitors"] = 0;
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            // Код, выполняемый при запуске нового сеанса
+            // Increment Visitors counter
+            Application["Visitors"] = long.Parse(Application["Visitors"].ToString()) + 1;
+        }
+    }
+}

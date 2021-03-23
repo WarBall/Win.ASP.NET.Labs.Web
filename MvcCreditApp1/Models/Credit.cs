b32a7747f9abbf869049c68eebe007b49fe097c0
WarBall <desik_t9@mail.ru>
2021-03-23 21:19:56 +0300

    Add new project

---
+++ MvcCreditApp1/Models/Credit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace MvcCreditApp1.Models
+{
+    public class Credit
+    {
+        // ID кредита
+        public virtual int CreditId { get; set; }
+
+        // Название
+        [Required]
+        [DisplayName("Название кредита")]
+        public virtual string Head { get; set; }
+
+        // Период, на который выдается кредит
+        [Required]
+        [DisplayName("Срок выдачи кредита")]
+        public virtual int Period { get; set; }
+
+        // Максимальная сумма кредита
+        [DisplayName("Сумма выдачи кредита")]
+        public virtual int Sum { get; set; }
+
+        // Процентная ставка
+        [Required]
+        [DisplayName("Процентная ставка")]
+        public virtual int Procent { get; set; }
+
+    }
+}
---
+++ MvcCreditApp1/Models/CreditContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+
+namespace MvcCreditApp1.Models
+{
+    //[Authorize(Roles = "admin")]
+    public class CreditContext : DbContext
+    {
+        public DbSet<Credit> Credits { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+    }
+}
